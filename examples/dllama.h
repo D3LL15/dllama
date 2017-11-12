ed32f1c087f7440677a60c3b78643aba5a075422
D3LL15 <daniel_ellis97@hotmail.com>
2017-11-12 17:28:12 +0000

    initial commit

---
+++ examples/dllama.h
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   dllama.h
+ * Author: dan
+ *
+ * Created on 07 November 2017, 16:54
+ */
+
+#ifndef DLLAMA_H
+#define DLLAMA_H
+
+#include "include/llama.h"
+
+class dllama {
+public:
+    dllama();
+    virtual ~dllama();
+    void test_llama_init();
+    void test_llama_interact();
+    void full_test();
+private:
+protected:
+    ll_writable_graph* graph;
+    ll_database database;
+};
+
+#endif /* DLLAMA_H */
+
---
+++ examples/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: dan
+ *
+ * Created on 29 October 2017, 15:42
+ */
+
+#include "dllama.h"
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    dllama x = dllama();
+    x.full_test();
+    return 0;
+}
+
