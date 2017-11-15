ea9088f4f853845b5bf01aead0de4aa0189199f6
D3LL15 <daniel_ellis97@hotmail.com>
2017-11-15 17:27:57 +0000

    made debugging easier

--- examples/main.cpp
+++ examples/main.cpp
@@ -18,10 +18,20 @@
  */
 int main(int argc, char** argv) {
     dllama x = dllama();
-    //x.full_test();
-    //x.test_llama_init();
+    if (argc == 2) {
+        switch (*argv[1]) {
+            case '1':
+                x.full_test();
+                break;
+            case '2':
+                x.test_llama_init();
+                break;
+            case '3':
+                x.test_llama_print_neighbours();
+                break;
+        }
+    }
     //x.test_llama_interact();
-    x.test_llama_print_neighbours();
     return 0;
 }
 
