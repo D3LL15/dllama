144f7d8c6ba72b22cba522f8c6b164a5f13ed205
Daniel Ellis <de300@cam.ac.uk>
2018-01-29 17:51:34 +0000

    fixed bug with neo4j benchmarks

--- src/main/java/Benchmark.java
+++ src/main/java/Benchmark.java
@@ -1,6 +1,7 @@
 import org.neo4j.driver.v1.*;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
@@ -18,13 +19,13 @@ public class Benchmark
 	}
 
 	private void addNodes() {
-		System.out.println("nanoseconds to add 10000 nodes");
+		System.out.println("nanoseconds to add 1000 nodes");
 		Session session = driver.session();
 		try {
-			for (int j = 0; j < 10; j++) {
+			for (int j = 0; j < 1; j++) {
 
 				long t1 = System.nanoTime();
-				for (int num = 0; num < 10000; num++) {
+				for (int num = 0; num < 1000; num++) {
 					session.run("CREATE (a:Node {num: {x}})", parameters("x", num));
 				}
 				long t2 = System.nanoTime();
@@ -39,17 +40,17 @@ public class Benchmark
 	}
 
 	private void addEdges() {
-		System.out.println("nanoseconds to add 100 edges to 10000 nodes");
+		System.out.println("nanoseconds to add 100 edges to 100 nodes");
 		Session session = driver.session();
 		try {
-			for (int j = 0; j < 10; j++) {
-				for (int num = 0; num < 10000; num++) {
+			for (int j = 0; j < 1; j++) {
+				for (int num = 0; num < 100; num++) {
 					session.run("CREATE (a:Node {num: {x}})", parameters("x", num));
 				}
 				long t1 = System.nanoTime();
-				for (int num = 0; num < 10000; num++) {
+				for (int num = 0; num < 100; num++) {
 					for (int i = 0; i < 100; i++) {
-						session.run("MATCH (a:Node), (b:node) WHERE a.num = {x} AND b.num = {y} CREATE (a)-(r:Edge)->(b)", parameters("x", num, "y", i));
+						session.run("MATCH (a:Node), (b:node) WHERE a.num = {x} AND b.num = {y} CREATE (a)-[r:Edge]->(b)", parameters("x", num, "y", i));
 					}
 				}
 				long t2 = System.nanoTime();
@@ -64,23 +65,23 @@ public class Benchmark
 	}
 
 	private void readEdges() {
-		System.out.println("nanoseconds to read 100 edges each from 10000 nodes");
+		System.out.println("nanoseconds to read 100 edges each from 100 nodes");
 		Session session = driver.session();
 		try {
-			for (int num = 0; num < 10000; num++) {
+			for (int num = 0; num < 100; num++) {
 				session.run("CREATE (a:Node {num: {x}})", parameters("x", num));
 			}
-			for (int num = 0; num < 10000; num++) {
+			for (int num = 0; num < 100; num++) {
 				for (int i = 0; i < 100; i++) {
-					session.run("MATCH (a:Node), (b:Node) WHERE a.num = {x} AND b.num = {y} CREATE (a)-(r:Edge)->(b)", parameters("x", num, "y", i));
+					session.run("MATCH (a:Node), (b:Node) WHERE a.num = {x} AND b.num = {y} CREATE (a)-[r:Edge]->(b)", parameters("x", num, "y", i));
 				}
 			}
-			for (int j = 0; j < 10; j++) {
+			for (int j = 0; j < 1; j++) {
 				long t1 = System.nanoTime();
 
-				for (int num = 0; num < 10000; num++) {
+				for (int num = 0; num < 100; num++) {
 					StatementResult result = session.run(
-							"MATCH (a:Node)-(r:Edge)->(b:Node) WHERE a.num = {x} RETURN b.num AS number",
+							"MATCH (a:Node)-[r:Edge]->(b:Node) WHERE a.num = {x} RETURN b.num AS number",
 							parameters("x", num));
 					List<Record> results = result.list();
 				}
@@ -99,7 +100,7 @@ public class Benchmark
 	private void addAndReadGraph(int numberOfNodes, String fileName) {
 		Session session = driver.session();
 		try {
-			for (int j = 0; j < 10; j++) {
+			for (int j = 0; j < 1; j++) {
 
 				BufferedReader reader;
 				reader = new BufferedReader(new FileReader(fileName));
@@ -114,13 +115,13 @@ public class Benchmark
 				String[] nodes;
 				while (line != null) {
 					nodes = line.split("	");
-					session.run("MATCH (a:Node), (b:node) WHERE a.num = " + nodes[0] + " AND b.num = " + nodes[1] + " CREATE (a)-(r:Edge)->(b)");
+					session.run("MATCH (a:Node), (b:node) WHERE a.num = " + nodes[0] + " AND b.num = " + nodes[1] + " CREATE (a)-[r:Edge]->(b)");
 					line = reader.readLine();
 				}
 
 				for (int num = 0; num < numberOfNodes; num++) {
 					StatementResult result = session.run(
-							"MATCH (a:Node)-(r:Edge)->(b:Node) WHERE a.num = {x} RETURN b.num AS number",
+							"MATCH (a:Node)-[r:Edge]->(b:Node) WHERE a.num = {x} RETURN b.num AS number",
 							parameters("x", num));
 					List<Record> results = result.list();
 				}
@@ -141,44 +142,12 @@ public class Benchmark
 
 	private void addAndReadPowerGraph() {
 		System.out.println("nanoseconds to add power law graph then read all edges from each node");
-		addAndReadGraph(1024, "kronecker_graph.net");
+		addAndReadGraph(1024, "dllama/kronecker_graph.net");
 	}
 
 	private void addAndReadKroneckerGraph() {
 		System.out.println("nanoseconds to add kronecker graph then read all edges from each node");
-		addAndReadGraph(1000, "powerlaw.net");
-	}
-
-	private void addPerson(String name)
-	{
-		// Sessions are lightweight and disposable connection wrappers.
-		Session session = driver.session();
-		// Wrapping Cypher in an explicit transaction provides atomicity
-		// and makes handling errors much easier.
-		Transaction tx = session.beginTransaction();
-		try {
-			tx.run("MERGE (a:Person {name: {x}})", parameters("x", name));
-			tx.success();  // Mark this write as successful.
-		} finally {
-			tx.close();
-		}
-	}
-
-	private void printPeople(String initial)
-	{
-		Session session = driver.session();
-		// Auto-commit transactions are a quick and easy way to wrap a read.
-		StatementResult result = session.run(
-				"MATCH (a:Person) WHERE a.name STARTS WITH {x} RETURN a.name AS name",
-				parameters("x", initial));
-		// Each Cypher execution returns a stream of records.
-		while (result.hasNext())
-		{
-			Record record = result.next();
-			//use list function to get actual list, otherwise lazy
-			// Values can be extracted from a record by index or name.
-			System.out.println(record.get("name").asString());
-		}
+		addAndReadGraph(1000, "dllama/powerlaw.net");
 	}
 
 	public void close()
@@ -187,19 +156,29 @@ public class Benchmark
 		driver.close();
 	}
 
+	public void deleteDatabase() {
+		Session session = driver.session();
+		session.run("MATCH (n:Node) DETACH DELETE n");
+	}
+
+	public void addIndex() {
+		Session session = driver.session();
+		session.run("CREATE INDEX ON :Node(num)");
+	}
+
 	public static void main(String... args)
 	{
 		Benchmark example = new Benchmark("bolt://localhost:7687", "neo4j", "password");
-		example.addPerson("Ada");
-		example.addPerson("Alice");
-		example.addPerson("Bob");
-		example.printPeople("A");
-		example.close();
+		example.addIndex();
 
-		/*example.addNodes();
+		//example.deleteDatabase();
+
+		example.addNodes();
 		example.addEdges();
 		example.readEdges();
 		example.addAndReadKroneckerGraph();
-		example.addAndReadPowerGraph();*/
+		example.addAndReadPowerGraph();
+
+		example.close();
 	}
 }
